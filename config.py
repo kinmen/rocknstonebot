c45a8738fc1f1f3bdfa0170b084615985be42b19
kinmen <kinmenyang7589@gmail.com>
2022-07-12 22:08:15 -0700

    Add a few more quotes

--- config.py
+++ config.py
@@ -12,8 +12,14 @@ REPLIES = [
     "Rock and Stone, Brother!",
     "Rock and Stone everyone!",
     "Rock and Stone forever!",
+    "Rock and roll and stone!",
+    "Rock and Stone to the Bone!",
+    "Rock and Stone in the Heart!",
     "For Rock and Stone!",
     "If you don't Rock and Stone, you ain't comin' home!",
     "Did I hear a Rock and Stone?",
-    "Rock and Stone to the Bone!"
+    "To Rock and Stone!",
+    "That's it lads! Rock and Stone!",
+    "We fight for Rock and Stone!",
+    "For Karl!"
     ]
