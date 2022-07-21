a23359d6a94755538f71a81836c9f4275a6a5848
kinmen <kinmenyang7589@gmail.com>
2022-07-20 17:42:23 -0700

    Dont reply in comment thread that we've already replied to

--- main.py
+++ main.py
@@ -45,15 +45,40 @@ def main():
             comment.reply(body = random.choice(config.REPLIES))
             comment.save() # We use save to save state and mark that we've already replied to this comment
 
-def should_reply(comment: str) -> bool:
+def should_reply(comment) -> bool:
     trig_re = r'.*rock and stone.*'
     text = comment.body
 
     # Check if matches rock and stone, not our comment (or from ignored user), and not one we've seen before
     if (re.match(trig_re, text, re.IGNORECASE)
         and comment.author.name not in config.IGNORED_USERS
-        and not comment.saved):
+        and not already_replied_comment(comment)
+        and not already_replied_thread(comment)):
         return True
+    return False
+
+def already_replied_comment(comment):
+    return comment.saved
+
+def already_replied_thread(comment):
+    if comment.saved:
+        return True
+
+    # Efficient way of discovering top level comment per:
+    # https://praw.readthedocs.io/en/stable/code_overview/models/comment.html#praw.models.Comment.parent
+    refresh_counter = 0
+    ancestor = comment
+    while not ancestor.is_root:
+        ancestor = ancestor.parent()
+
+        if refresh_counter % 9 == 0:
+            ancestor.refresh()
+        refresh_counter += 1
+
+        if already_replied_comment(ancestor):
+            logger.info("Already replied to comment: {}. Replied comment ancestor: {}".format(comment.id, ancestor.id))
+            return True
+
     return False
 
 if __name__ == '__main__':
